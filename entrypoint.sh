b46098394263eea1d2f7564607b60666e8891471
Simon Hookway <simon@obsidian.com.au>
2018-12-18 14:14:47 +1100

    updated

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
+# Read environment variables from file and export them.
+file_env() {
+    while read -r line || [[ -n $line ]]; do
+        export $line
+    done < "$1"
+}
+
 source /home/jet/cfg/defaults
 source /home/jet/cfg/build-args
 if [ -e /home/jet/cfg/$CLIENT ]; then
     source /home/jet/cfg/$CLIENT
+fi
+
+JETDB_IP=`dig +short -t SRV db.local | awk '{ print $4 }'`
+FILETRACKER_IP=`dig +short -t SRV filetracker.local | awk '{ print $4 }'`
+export $JETDB_IP $FILETRACKER_IP
+
+if [ -f /root/environment_vars ]; then
+    file_env /root/environment_vars
 fi
 
 if [ -e /var/run/supervisord.pid ]; then
@@ -16,7 +31,35 @@ if [ ! -e /home/jet/.jet.cf ]; then
     sudo -u jet /home/jet/bin/setup-jet.sh
 fi
 
-echo "Commands: " $@
+if [ -e /opt/ramdisk ]; then
+    if [ ! -e /opt/ramdisk/db ]; then
+        mkdir /opt/ramdisk/db
+    fi
+    chown -R jet:jet /opt/ramdisk/db
+fi
+
+echo "Running dynamic update."
+chmod 755 /opt/patches/runContainer.sh
+/opt/patches/runContainer.sh
+
+echo "Setting up environment variables for Jet."
+python /home/jet/bin/setup-env.py
+chown jet:jet /home/jet/Jet/.env
+
+echo "Running jet pre-supervisor startup script."
+sudo -u jet /home/jet/bin/startup.sh
 
 service supervisor start
+supervisorctl start $1
+
+term_handler() {
+    supervisorctl stop all
+    service supervisor stop
+    exit 0;
+}
+
+trap 'term_handle' SIGTERM
+
+while true; do :; done
+
 exit 0
